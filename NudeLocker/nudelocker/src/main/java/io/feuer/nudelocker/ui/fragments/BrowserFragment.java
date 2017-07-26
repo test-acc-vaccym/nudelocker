59d5b8f318ce3cc3bde0ca848dd2259160a8bae7
Jannik <Jannik.Feuerhahn@gmx.de>
2017-07-26 22:44:34 +0200

    added - more FAB stubs

--- NudeLocker/nudelocker/src/main/java/io/feuer/nudelocker/ui/fragments/BrowserFragment.java
+++ NudeLocker/nudelocker/src/main/java/io/feuer/nudelocker/ui/fragments/BrowserFragment.java
@@ -10,6 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +25,7 @@ import io.feuer.nudelocker.ui.adapters.BrowserAdapter;
  * Das Hauptfragment. Beinhaltet den "Dateibrowser"
  * Created by jannik@feuer.io on 19.07.2017.
  */
-public class BrowserFragment extends JFragment {
+public class BrowserFragment extends JFragment implements View.OnClickListener {
 
     /**
      * RecyclerView
@@ -45,6 +48,21 @@ public class BrowserFragment extends JFragment {
     private @Nullable BrowserAdapter adapter;
 
     /**
+     * FAB-Menu
+     */
+    private @Nullable FloatingActionMenu famAdd;
+
+    /**
+     * Fab Neuer Ordner
+     */
+    private @Nullable FloatingActionButton fabCreateFolder;
+
+    /**
+     * Fab Import Bild
+     */
+    private @Nullable FloatingActionButton fabImportImage;
+
+    /**
      * Liste der Elemente
      */
     private @Nullable List<BrowserItem> items;
@@ -53,9 +71,8 @@ public class BrowserFragment extends JFragment {
      * Konstruktor
      */
     public BrowserFragment() {
-        //items.addAll(BrowserItem.getDefaultItems(getContext()));
+        //
     }
-
 
     @Nullable
     @Override
@@ -64,6 +81,10 @@ public class BrowserFragment extends JFragment {
         final View view = inflater.inflate(R.layout.lay_fragment_browser, container, false);
 
         rvBrowser = (RecyclerView) view.findViewById(R.id.rvBrowser);
+        famAdd = (FloatingActionMenu) view.findViewById(R.id.famAdd);
+        fabCreateFolder = (FloatingActionButton) view.findViewById(R.id.fabCreateFolder);
+        fabImportImage = (FloatingActionButton) view.findViewById(R.id.fabImportImage);
+
 
         lmLinear = new LinearLayoutManager(getContext());
         lmGrid = new GridLayoutManager(getContext(), GridLayoutManager.DEFAULT_SPAN_COUNT);
@@ -71,6 +92,15 @@ public class BrowserFragment extends JFragment {
 
         if(rvBrowser != null) {
             rvBrowser.setLayoutManager(lmLinear);
+        }
+
+        if(famAdd != null) {
+            famAdd.setClosedOnTouchOutside(true);
+        }
+
+        if(fabImportImage != null && fabCreateFolder != null) {
+            fabImportImage.setOnClickListener(this);
+            fabCreateFolder.setOnClickListener(this);
         }
 
         return view;
@@ -81,6 +111,17 @@ public class BrowserFragment extends JFragment {
         super.onResume();
         if(items == null) {
             items = new ArrayList<>(BrowserItem.getDefaultItems(getContext()));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view != null) {
+            if(view == fabCreateFolder) {
+
+            } else if(view == fabImportImage) {
+
+            }
         }
     }
 }
